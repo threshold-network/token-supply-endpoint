80d20e5d397edeb0e21ac67c38890183154d47ec
derekpierre <derek.pierre@gmail.com>
2023-02-02 16:05:54 -0500

    Various updates including: - use of staking rewards summary file in merke-distribution repos - use common erc20 abi file - dao treasury supply should not include overminted supply - add overminted supply value to response

--- t-circulating-supply/main.py
+++ t-circulating-supply/main.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime
 
 import requests
 from web3.auto import w3
@@ -9,12 +8,12 @@ NU_TOKEN_FACTOR = 3.25924249316
 KEEP_TOKEN_FACTOR = 4.78318863126
 
 # Addresses
-T_TOKEN_ADDRESS = '0xCdF7028ceAB81fA0C6971208e83fa7872994beE5'
-T_TREASURY_ADDRESS = '0x9F6e831c8F8939DC0C830C6e492e7cEf4f9C2F5f'
-KEEP_TOKEN_ADDRESS = '0x85Eee30c52B0b379b046Fb0F85F4f3Dc3009aFEC'
-KEEP_TOKEN_GRANT_ADDRESS = '0x175989c71Fd023D580C65F5dC214002687ff88B7'
-NU_TOKEN_ADDRESS = '0x4fE83213D56308330EC302a8BD641f1d0113A4Cc'
-MERKLE_DISTRIBUTION_ADDRESS = '0xeA7CA290c7811d1cC2e79f8d706bD05d8280BD37'
+T_TOKEN_ADDRESS = "0xCdF7028ceAB81fA0C6971208e83fa7872994beE5"
+T_TREASURY_ADDRESS = "0x9F6e831c8F8939DC0C830C6e492e7cEf4f9C2F5f"
+KEEP_TOKEN_ADDRESS = "0x85Eee30c52B0b379b046Fb0F85F4f3Dc3009aFEC"
+KEEP_TOKEN_GRANT_ADDRESS = "0x175989c71Fd023D580C65F5dC214002687ff88B7"
+NU_TOKEN_ADDRESS = "0x4fE83213D56308330EC302a8BD641f1d0113A4Cc"
+MERKLE_DISTRIBUTION_ADDRESS = "0xeA7CA290c7811d1cC2e79f8d706bD05d8280BD37"
 
 # Token supply constants
 MAGIC_KEEP_SUPPLY_SUBTRACTOR = 59053770 * (10 ** 18)
@@ -22,10 +21,15 @@ INITIAL_T_SUPPLY = 10_000_000_000 * (10 ** 18)  # 10B T
 INITIAL_TREASURY_SUPPLY = 1_000_000_000 * (10 ** 18)  # 1B T
 
 # Merkle Distribution
-MERKLE_DISTRIBUTION_FILENAME = "MerkleDist.json"
+MERKLE_DISTRIBUTION_SUMMARY_FILENAME = "distributions.json"
+MERKLE_DISTRIBUTION_CONTRACT_DEPLOYMENT_BLOCK = 15146501  # block that contract was deployed
 
 
 def make_request(request_url: str, verify: bool = True):
+    """
+    Issues a HTTP GET for the provided request url. If successful, the response data
+    is expected to be JSON.
+    """
     response = requests.get(request_url, verify=verify)
     if response.status_code != 200:
         # TODO is this the best way to indicate an error occurred
@@ -35,41 +39,28 @@ def make_request(request_url: str, verify: bool = True):
 
 
 def get_total_staking_rewards():
-
-    # TODO only use latest distribution (it has cumulative total)
-
-    # TODO may need GITHUB TOKEN to prevent rate limiting
-    distribution_folders = make_request(
-        "https://api.github.com/repos/threshold-network/merkle-distribution/contents/distributions"
-    )
-
-    # get folder names
-    distribution_folder_names = []
-    for folder in distribution_folders:
-        distribution_folder_names.append(folder['name'])
-
-    # sort folder names by date
-    distribution_folder_names.sort(key=lambda date: datetime.strptime(date, "%Y-%m-%d"))
-
-    latest_folder = distribution_folder_names[-1]
-
+    """
+    Retrieves total staking rewards issued from the threshold/merkle-distribution repos.
+    """
     distribution_file = make_request(
-        f"https://raw.githubusercontent.com/threshold-network/merkle-distribution/main/distributions/{latest_folder}/{MERKLE_DISTRIBUTION_FILENAME}"
+        f"https://raw.githubusercontent.com/threshold-network/merkle-distribution/main/distributions/{MERKLE_DISTRIBUTION_SUMMARY_FILENAME}"
     )
-    total_rewards_for_distribution = int(distribution_file['totalAmount'])
-
-    return total_rewards_for_distribution
+    total_staking_rewards = int(distribution_file["LatestCumulativeAmount"])
+    return total_staking_rewards
 
 
 def get_already_claimed_rewards():
+    """
+    Determined the amount of rewards already claimed from the MerkleDistribution contract.
+    """
     # TODO would be great to cache the return of this function - doesn't need to be done every time.
     total_claimed = 0
-    with open('merkle-distribution.abi') as merkle_distribution_contract_file:
+    with open("merkle-distribution.abi") as merkle_distribution_contract_file:
         abi = merkle_distribution_contract_file.read()
         merkle_distribution_contract = w3.eth.contract(address=MERKLE_DISTRIBUTION_ADDRESS, abi=abi)
-        events = merkle_distribution_contract.events['Claimed'].getLogs(fromBlock=15146501)  # block that contract was deployed
+        events = merkle_distribution_contract.events["Claimed"].getLogs(fromBlock=MERKLE_DISTRIBUTION_CONTRACT_DEPLOYMENT_BLOCK)
         for event in events:
-            total_claimed += event['args']['amount']
+            total_claimed += event["args"]["amount"]
 
     return total_claimed
 
@@ -87,13 +78,14 @@ def main(request):
     #
     # Total Supply Calcs
     #
-    with open('t-contract.abi') as t_contract_file:
-        abi = t_contract_file.read()
-        t_token_contract = w3.eth.contract(address=T_TOKEN_ADDRESS, abi=abi)
-        t_total_supply = t_token_contract.functions.totalSupply().call()
+    with open("erc20.abi") as erc20_abi_file:
+        abi = erc20_abi_file.read()
 
-        if request.path == "/total":
-            return str(t_total_supply / (10 ** 18))
+    t_token_contract = w3.eth.contract(address=T_TOKEN_ADDRESS, abi=abi)
+    t_total_supply = t_token_contract.functions.totalSupply().call()
+
+    if request.path == "/total":
+        return str(t_total_supply / (10 ** 18))
 
     #
     # Treasury Supply Calcs
@@ -103,10 +95,12 @@ def main(request):
     total_staking_rewards = get_total_staking_rewards()
     claimed_rewards = get_already_claimed_rewards()
     unclaimed_rewards = total_staking_rewards - claimed_rewards
+    total_minted_supply = t_total_supply - INITIAL_T_SUPPLY  # staking rewards + overmint
+    overminted_tokens = (total_minted_supply - total_staking_rewards)
 
-    # Treasury Supply i.e. unused T (including overminted tokens) = CurrentWalletBalance - unclaimed rewards
+    # Treasury Supply i.e. unused T (excluding overminted tokens) = CurrentWalletBalance - unclaimed rewards - overminted tokens
     t_treasury_current_balance = t_token_contract.functions.balanceOf(T_TREASURY_ADDRESS).call()
-    t_treasury_supply = t_treasury_current_balance - unclaimed_rewards
+    t_treasury_supply = t_treasury_current_balance - unclaimed_rewards - overminted_tokens
 
     if request.path == "/treasury":
         return str(t_treasury_supply / (10 ** 18))
@@ -116,10 +110,8 @@ def main(request):
     #
 
     # KEEP Tokens Calc
-    with open('keep-token.abi') as keep_contract_file:
-        abi = keep_contract_file.read()
-        keep_token_contract = w3.eth.contract(address=KEEP_TOKEN_ADDRESS, abi=abi)
-        keep_total_supply = keep_token_contract.functions.totalSupply().call()
+    keep_token_contract = w3.eth.contract(address=KEEP_TOKEN_ADDRESS, abi=abi)
+    keep_total_supply = keep_token_contract.functions.totalSupply().call()
 
     keep_token_grant = keep_token_contract.functions.balanceOf(
         KEEP_TOKEN_GRANT_ADDRESS).call()
@@ -133,24 +125,24 @@ def main(request):
     )
     circulating_t_from_nu = nu_circulating_supply * NU_TOKEN_FACTOR * (10 ** 18)
 
-    # Treasury spend = Initial Treasury Supply - (CurrentWalletBalance - unclaimed rewards - overminted tokens)
-    total_minted_supply = t_total_supply - INITIAL_T_SUPPLY  # staking rewards + overmint
-    overminted_tokens = (total_minted_supply - total_staking_rewards)
-    t_treasury_spend = INITIAL_TREASURY_SUPPLY - (
-                t_treasury_current_balance - unclaimed_rewards - overminted_tokens)
+    # Treasury spend = Initial Treasury Supply - Current Treasury Supply
+    t_treasury_spend = INITIAL_TREASURY_SUPPLY - t_treasury_supply
 
-    # T circulating supply = All unlocked NU * 3.259 + All unlocked KEEP * 4.783 + staking rewards + treasury spend
+    # T circulating supply
+    #     = All unlocked NU * 3.259 + All unlocked KEEP * 4.783 + staking rewards + treasury spend
     circulating_t = circulating_t_from_nu + circulating_t_from_keep + total_staking_rewards + t_treasury_spend
     circulating_t_tokens = circulating_t / (10 ** 18)
+
     results = {
-        'current_total_supply': t_total_supply / (10 ** 18),
-        'dao_treasury_supply': t_treasury_supply / (10 ** 18),
-        'est_circulating_supply': circulating_t_tokens
+        "current_total_supply": t_total_supply / (10 ** 18),
+        "dao_treasury_supply": t_treasury_supply / (10 ** 18),
+        "future_rewards_supply": overminted_tokens / (10**18),
+        "est_circulating_supply": circulating_t_tokens
     }
 
     if request.path == "/circulating":
         return str(circulating_t_tokens)
-    elif request.path == '/':
+    elif request.path == "/":
         return json.dumps(results)
     else:
-        raise Exception(f'Unknown route {request.path}')
+        raise Exception(f"Unknown route {request.path}")
