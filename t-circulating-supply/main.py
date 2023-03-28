f635575558c27b29296a005c837caac4dd65c2af
derekpierre <derek.pierre@gmail.com>
2023-03-28 10:18:36 -0400

    Use GB Timelock contract for treasury calculation.

--- t-circulating-supply/main.py
+++ t-circulating-supply/main.py
@@ -4,18 +4,16 @@ import requests
 from web3.auto import w3
 
 # Token factors
-NU_TOKEN_FACTOR = 3.25924249316
-KEEP_TOKEN_FACTOR = 4.78318863126
+NU_TOKEN_FACTOR = 3.25924249316  # NU -> T
+KEEP_TOKEN_FACTOR = 4.78318863126  # KEEP -> T
 
 WEI_FACTOR = 10 ** 18
 
 # Addresses
 T_TOKEN_ADDRESS = "0xCdF7028ceAB81fA0C6971208e83fa7872994beE5"
-T_TREASURY_ADDRESS = "0x9F6e831c8F8939DC0C830C6e492e7cEf4f9C2F5f"
 KEEP_TOKEN_ADDRESS = "0x85Eee30c52B0b379b046Fb0F85F4f3Dc3009aFEC"
 KEEP_TOKEN_GRANT_ADDRESS = "0x175989c71Fd023D580C65F5dC214002687ff88B7"
-NU_TOKEN_ADDRESS = "0x4fE83213D56308330EC302a8BD641f1d0113A4Cc"
-MERKLE_DISTRIBUTION_ADDRESS = "0xeA7CA290c7811d1cC2e79f8d706bD05d8280BD37"
+GB_TIMELOCK_ADDRESS = "0x87F005317692D05BAA4193AB0c961c69e175f45f"  # T Treasury stored here
 
 # Token supply constants
 MAGIC_KEEP_SUPPLY_SUBTRACTOR = 59053770 * WEI_FACTOR
@@ -26,12 +24,11 @@ NU_CIRCULATING_SUPPLY_ENDPOINT = "https://status.nucypher.network/supply_informa
 
 # Merkle Distribution
 MERKLE_DISTRIBUTION_SUMMARY_ENDPOINT = f"https://raw.githubusercontent.com/threshold-network/merkle-distribution/main/distributions/distributions.json"
-MERKLE_DISTRIBUTION_CONTRACT_DEPLOYMENT_BLOCK = 15146501  # block that contract was deployed
 
 
 def make_request(request_url: str, verify: bool = True):
     """
-    Issues a HTTP GET for the provided request url. If successful, the response data
+    Issues an HTTP GET for the provided request url. If successful, the response data
     is expected to be JSON.
     """
     response = requests.get(request_url, verify=verify)
@@ -49,24 +46,6 @@ def get_total_staking_rewards():
     distribution_file = make_request(MERKLE_DISTRIBUTION_SUMMARY_ENDPOINT)
     total_staking_rewards = int(distribution_file["LatestCumulativeAmount"])
     return total_staking_rewards
-
-
-def get_already_claimed_rewards():
-    """
-    Determined the amount of rewards already claimed from the MerkleDistribution contract.
-    """
-    # TODO would be great to cache the return of this function - doesn't need to be done every time.
-    total_claimed = 0
-    with open("merkle-distribution.abi") as merkle_distribution_contract_file:
-        abi = merkle_distribution_contract_file.read()
-        merkle_distribution_contract = w3.eth.contract(address=MERKLE_DISTRIBUTION_ADDRESS, abi=abi)
-        events = merkle_distribution_contract.events["Claimed"].getLogs(
-            fromBlock=MERKLE_DISTRIBUTION_CONTRACT_DEPLOYMENT_BLOCK
-        )
-        for event in events:
-            total_claimed += event["args"]["amount"]
-
-    return total_claimed
 
 
 def main(request):
@@ -95,23 +74,18 @@ def main(request):
     # Treasury Supply Calcs
     #
     
-    # Staking Rewards
-    total_staking_rewards = get_total_staking_rewards()
-    claimed_rewards = get_already_claimed_rewards()
-    unclaimed_rewards = total_staking_rewards - claimed_rewards
-    total_minted_supply = t_total_supply - INITIAL_T_SUPPLY  # staking rewards + overmint
-    overminted_tokens = (total_minted_supply - total_staking_rewards)
-
-    # Treasury Supply i.e. unused T (excluding overminted tokens) = CurrentWalletBalance - unclaimed rewards - overminted tokens
-    t_treasury_current_balance = t_token_contract.functions.balanceOf(T_TREASURY_ADDRESS).call()
-    t_treasury_supply = t_treasury_current_balance - unclaimed_rewards - overminted_tokens
-
+    # Treasury Supply i.e. T in Governor Bravo TimeLock Contract
+    t_treasury_supply = t_token_contract.functions.balanceOf(GB_TIMELOCK_ADDRESS).call()
     if request.path == "/treasury":
         return str(t_treasury_supply / WEI_FACTOR)
 
     #
     # Circulating Supply Calcs
     #
+
+    # NU Tokens Calc
+    nu_circulating_supply = make_request(NU_CIRCULATING_SUPPLY_ENDPOINT, verify=False)
+    circulating_t_from_nu = nu_circulating_supply * NU_TOKEN_FACTOR * WEI_FACTOR
 
     # KEEP Tokens Calc
     keep_token_contract = w3.eth.contract(address=KEEP_TOKEN_ADDRESS, abi=abi)
@@ -122,9 +96,10 @@ def main(request):
     keep_circulating_supply = keep_total_supply - keep_token_grant - MAGIC_KEEP_SUPPLY_SUBTRACTOR
     circulating_t_from_keep = keep_circulating_supply * KEEP_TOKEN_FACTOR
 
-    # NU Tokens Calc
-    nu_circulating_supply = make_request(NU_CIRCULATING_SUPPLY_ENDPOINT, verify=False)
-    circulating_t_from_nu = nu_circulating_supply * NU_TOKEN_FACTOR * WEI_FACTOR
+    # Staking Rewards
+    total_staking_rewards = get_total_staking_rewards()
+    total_minted_supply = t_total_supply - INITIAL_T_SUPPLY  # claimable staking rewards + future rewards
+    future_rewards = (total_minted_supply - total_staking_rewards)
 
     # Treasury spend = Initial Treasury Supply - Current Treasury Supply
     t_treasury_spend = INITIAL_TREASURY_SUPPLY - t_treasury_supply
@@ -137,7 +112,7 @@ def main(request):
     results = {
         "current_total_supply": t_total_supply / WEI_FACTOR,
         "dao_treasury_supply": t_treasury_supply / WEI_FACTOR,
-        "future_rewards_supply": overminted_tokens / WEI_FACTOR,
+        "future_rewards_supply": future_rewards / WEI_FACTOR,
         "est_circulating_supply": circulating_t_tokens
     }
 
